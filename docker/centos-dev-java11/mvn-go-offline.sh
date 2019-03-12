2d4c6ab22034f1d6a553efbc2ccc94590bfa9198
Michael Vorburger <vorburger@redhat.com>
2019-03-13 00:18:13 +0100

    add 'clean' in mvn-go-offline.sh

--- docker/centos-dev-java11/mvn-go-offline.sh
+++ docker/centos-dev-java11/mvn-go-offline.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Running Maven once during container creation already pre-loads Maven dependencies
 # into ~/.m2, which is very useful to start faster when this dev container will start IRL.
-mvn compile dependency:go-offline dependency:resolve-plugins
+mvn clean compile dependency:go-offline dependency:resolve-plugins
 
 # Due to https://issues.apache.org/jira/browse/MDEP-82, the above does not actually grab
 # all of quarkus-maven-plugin dependencies, so we now also run "mvn quarkus:dev"
