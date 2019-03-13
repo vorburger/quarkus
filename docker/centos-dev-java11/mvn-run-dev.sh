7e45c5afa730a1c34d67cce734efd69d2588832c
Michael Vorburger <vorburger@redhat.com>
2019-03-13 02:40:41 +0100

    use -Dquarkus.http.host=0.0.0.0 in mvn-run-dev.sh

--- docker/centos-dev-java11/mvn-run-dev.sh
+++ docker/centos-dev-java11/mvn-run-dev.sh
@@ -3,4 +3,5 @@ set -ex
 
 # TODO Come up with some black magic whichcraft in mvn-run-dev.sh which restarts 'mvn compile quarkus:dev' whenever the pom.xml changes...
 
-mvn compile quarkus:dev
+mvn compile quarkus:dev -Dquarkus.http.host=0.0.0.0
+
